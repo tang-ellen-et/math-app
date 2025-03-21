b58a5b3b99170697d2f7acfe3e9144be40cb2b7b
tange <ellen.tang@moodys.com>
2025-03-20 21:13:49 -0700

    updated the layout by cursor

--- mathapp/pages/userdashboard.py
+++ mathapp/pages/userdashboard.py
@@ -5,22 +5,39 @@ from mathapp.state import State
 def userdashboard():
     return rx.box(
         rx.section(
-            rx.center(rx.heading("User Exercise Dashboard", size="9", bold=True, color_scheme="blue")) ,
+            rx.center(rx.heading("User Exercise Dashboard", size="9", bold=True, color_scheme="blue")),
             rx.center(rx.link("Home", href="/", size="8", bold=True, color_scheme="blue")),
-            background="right/cover url('/math_app_logo.png')"
+            background="right/cover url('/math_app_logo.png')",
+            padding="2em"
         ),
         rx.section(
-            UserStats.graph_table(State.items_by_result)
-    
-        ),
-        rx.section(
-                rx.flex (
-                UserStats.graph_pie( State.items_by_result),
-                UserStats.graph(State.items_by_type)
+            rx.flex(
+                rx.box(
+                    UserStats.graph_table(State.items_by_result),
+                    width="100%",
+                    padding="1em"
+                ),
+                width="100%",
+                justify="center"
             )
         ),
-
-        
-        width = "70%"
-        
+        rx.section(
+            rx.flex(
+                rx.box(
+                    UserStats.graph_pie(State.items_by_result),
+                    width="50%",
+                    padding="1em"
+                ),
+                rx.box(
+                    UserStats.graph(State.items_by_type),
+                    width="50%", 
+                    padding="1em"
+                ),
+                width="100%",
+                justify="center",
+                align="center"
+            )
+        ),
+        width="80%",
+        margin="0 auto"
     )
