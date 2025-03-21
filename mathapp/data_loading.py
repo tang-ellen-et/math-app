6a3e650f3461b6ece377046131114a958babac1d
tange <ellen.tang@moodys.com>
2025-03-20 21:21:35 -0700

    clean up of data loading part

--- mathapp/data_loading.py
+++ mathapp/data_loading.py
@@ -1,68 +1,50 @@
 import csv
 import pandas as pd
-from datetime import date, datetime
+from datetime import datetime
 from mathapp.utils import get_aime_problems
-
 import reflex as rx
 
-def add_csv_data_to_db(data_file_path: str, model: rx.Model):
+def add_csv_data_to_db(data_file_path: str, model: rx.Model) -> None:
     with open(data_file_path, mode="r", newline="", encoding="utf-8") as file:
-        reader = csv.DictReader(
-            file
-        )  # This automatically uses the first row as header names
-
+        reader = csv.DictReader(file)
         with rx.session() as session:
             for row in reader:
-                # Create an instance of Model using dictionary unpacking
-                item = model(**row)
-                session.add(item)
+                session.add(model(**row))
             session.commit()
 
-
-def add_pandas_data_to_db(df: pd.DataFrame, model: rx.Model):  
+def add_pandas_data_to_db(df: pd.DataFrame, model: rx.Model) -> None:
     with rx.session() as session:
-        for index, row in df.iterrows():
-            # Create an instance of Model using dictionary unpacking
-            data_tuple = row.to_dict()
-            item = model(**data_tuple)
-            session.add(item)
+        for _, row in df.iterrows():
+            session.add(model(**row.to_dict()))
         session.commit()
 
-
 def generate_user_problem_sets(user: str, problem_set: str, problems_df: pd.DataFrame) -> pd.DataFrame:
-    
     test_date = datetime.today()
-    # problem_set = str(round(datetime.now().timestamp() * 1000))
+    problems_df['pid'] = problems_df.index + 1
     
-    problems_df['pid'] = problems_df.index + 1 
-
-    # Randomly select 20 problems from problems_df with different difficulty levels
-    # user_problems_df = problems_df
     user_problems_df = get_aime_problems(problems_df, difficulty=5)
-    # print(user_problems_df.head(2))
     
-    user_problems_df ['User'] = user
-    user_problems_df['ProblemSet'] = problem_set 
-    user_problems_df["ProblemId"] = problems_df['pid']
-    user_problems_df["TestDate"] = test_date.strftime("%Y-%m-%d")
-    user_problems_df["Response"] = '' 
-    user_problems_df["Result"] = '' 
-
+    user_problems_df.assign(
+        User=user,
+        ProblemSet=problem_set,
+        ProblemId=problems_df['pid'],
+        TestDate=test_date.strftime("%Y-%m-%d"),
+        Response='',
+        Result=''
+    )
+    
     return user_problems_df
 
-def load_all_problems(data_file_path :str, math_model: rx.Model, load_to_db: bool = False)->pd.DataFrame:
+def load_all_problems(data_file_path: str, math_model: rx.Model, load_to_db: bool = False) -> pd.DataFrame:
     df_problems = pd.read_csv(data_file_path, header=0)
     print(f'====== df columns: {df_problems.columns} ===')
     
     if load_to_db:
         add_pandas_data_to_db(df_problems, math_model)
-    return df_problems 
+    return df_problems
 
-def load_user_problems(user: str,  df_problems: pd.DataFrame, user_problems_model: rx.Model)-> str:
-    # df = pd.read_csv(data_file_path, header=0)
-    # print(f'====== df columns: {df.columns} ===')
+def load_user_problems(user: str, df_problems: pd.DataFrame, user_problems_model: rx.Model) -> str:
     problem_set = str(round(datetime.now().timestamp() * 1000))
-    user_problems_df = generate_user_problem_sets (user, problem_set, df_problems)
+    user_problems_df = generate_user_problem_sets(user, problem_set, df_problems)
     add_pandas_data_to_db(user_problems_df, user_problems_model)
     return problem_set
-    
